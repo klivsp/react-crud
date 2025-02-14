41f8d77a3b3fe618420e67db12b91341ebb2a040
Kliv Spahiu <135651423+klivsp@users.noreply.github.com>
2025-02-14 11:59:20 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { StrictMode } from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store.ts';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter /* basename="your-base-url-here" */>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
